a9ec52b03496acb6d5a8209c7575dca28b95246f
Otxoa Lardeux <olardeux@made-f0Cr3s1.clusters.42paris.fr>
2024-12-03 12:40:21 +0100

    fix syntax error

--- src/parsing/special_char.c
+++ src/parsing/special_char.c
@@ -6,7 +6,7 @@
 /*   By: olardeux <olardeux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 14:49:07 by olardeux          #+#    #+#             */
-/*   Updated: 2024/11/20 10:52:25 by olardeux         ###   ########.fr       */
+/*   Updated: 2024/12/03 12:00:03 by olardeux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	redirect_input(t_cmd_list *cmd, t_parsing *parsing, t_token **start)
 	t_redir	*redir;
 
 	if (!(*start)->next || (*start)->next->type != WORD)
-		return (error_msg(SYNTAX_ERROR, NULL), 0);
+		return (error_msg(SYNTAX_ERROR, (*start)->token), 0);
 	if ((*start)->token[1] == '<')
 	{
 		if (!redirect_heredoc(cmd, parsing, start))
@@ -68,7 +68,7 @@ int	redirect_append(t_cmd_list *cmd, t_parsing *parsing, t_token **start)
 
 	redir = init_redir();
 	if (!(*start)->next || (*start)->next->type != WORD)
-		return (error_msg(SYNTAX_ERROR, NULL), 0);
+		return (error_msg(SYNTAX_ERROR, (*start)->token), 0);
 	if (!set_redir(redir, cmd, *start))
 		return (0);
 	parsing->i += 2;
@@ -81,7 +81,7 @@ int	redirect_output(t_cmd_list *cmd, t_parsing *parsing, t_token **start)
 	t_redir	*redir;
 
 	if (!(*start)->next || (*start)->next->type != WORD)
-		return (error_msg(SYNTAX_ERROR, NULL), 0);
+		return (error_msg(SYNTAX_ERROR, (*start)->token), 0);
 	if ((*start)->token[1] == '>')
 	{
 		if (!redirect_append(cmd, parsing, start))
